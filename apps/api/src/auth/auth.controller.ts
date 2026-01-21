2f3c0c742b873e95495f00f07b1bf766a61fe124
Abdul Owhab <owhab.cse@gmail.com>
2026-01-21 16:58:55 +0600

    fix: update the provider from the provider enum

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './provider/auth.service';
 import { RegisterDto } from './dtos/register.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { Provider } from 'src/common/enums/provider.enum';
 
 @Controller('auth')
 export class AuthController {
@@ -13,28 +14,28 @@ export class AuthController {
     return this.authService.register(dto);
   }
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(AuthGuard(Provider.LOCAL))
   @Post('login')
   login(@Req() req) {
     return this.authService.generateToken(req.user);
   }
 
-  @Get('google')
-  @UseGuards(AuthGuard('google'))
+  @Get(Provider.GOOGLE)
+  @UseGuards(AuthGuard(Provider.GOOGLE))
   googleAuth() {}
 
   @Get('google/callback')
-  @UseGuards(AuthGuard('google'))
+  @UseGuards(AuthGuard(Provider.GOOGLE))
   googleCallback(@Req() req) {
     return this.authService.generateToken(req.user);
   }
 
-  @Get('github')
-  @UseGuards(AuthGuard('github'))
+  @Get(Provider.GITHUB)
+  @UseGuards(AuthGuard(Provider.GITHUB))
   githubAuth() {}
 
   @Get('github/callback')
-  @UseGuards(AuthGuard('github'))
+  @UseGuards(AuthGuard(Provider.GITHUB))
   githubCallback(@Req() req) {
     return this.authService.generateToken(req.user);
   }
